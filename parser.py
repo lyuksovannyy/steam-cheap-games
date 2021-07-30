2b093dbc8b8768af23a6388c1576326804d26607
GameSTALkER <52292220+GameSTALkER@users.noreply.github.com>
2021-07-30 12:21:27 +0300

    added ipvanish support & fixes
    added ip vanish proxy support
    some fixes

--- parser.py
+++ parser.py
@@ -11,80 +11,99 @@ from colorama import Fore, Style
 
 colorama.init()
 
-detalied_actions = True # print more information
-detailed_errors = True # print more info about errors, must be on "print_error"
-print_error = True # print errors
+detalied_actions = True 		# print more information
+detailed_errors = True 			# print more info about errors, must be on "print_error"
+print_error = True 				# print errors
+								
+currency = "ru" 				# recommended currency for that is Russian Ruble or Argentina Peso, by that you can buy games for lower price
+secondcurrency = 5 				# https://stackoverflow.com/a/65653682 
+currency_mark = "pуб."			
+pages = 3 						# how many pages will parsed
+page_from = 1 					# starting from page
+games_limit = 100 				# how many games you need to parse | 0 - until pages end.
+sleep_time = 1.3 				# time to next card info get
+								
+using_proxies = True			
+only_proxies = False 			# use this of you have good proxies or process will so slowly, for example ipvanish proxies (so good)
+IpVanish_using = False			
+# IPVanish creds #				
+if IpVanish_using is True: 		# creds in https://account.ipvanish.com/index.php?t=SOCKS5%20Proxy
+	ipvanish_hosts = ['syd.socks.ipvanish.com','gig.socks.ipvanish.com','tor.socks.ipvanish.com','par.socks.ipvanish.com','fra.socks.ipvanish.com','lin.socks.ipvanish.com','nrt.socks.ipvanish.com','ams.socks.ipvanish.com','waw.socks.ipvanish.com','lis.socks.ipvanish.com','sin.socks.ipvanish.com','mad.socks.ipvanish.com','sto.socks.ipvanish.com','lon.socks.ipvanish.com','iad.socks.ipvanish.com','atl.socks.ipvanish.com','bos.socks.ipvanish.com','clt.socks.ipvanish.com','chi.socks.ipvanish.com','dal.socks.ipvanish.com','den.socks.ipvanish.com','lax.socks.ipvanish.com','mia.socks.ipvanish.com','msy.socks.ipvanish.com','nyc.socks.ipvanish.com','phx.socks.ipvanish.com','sea.socks.ipvanish.com']
+	ipvanish_port = int('port')
+	ipvanish_username = "username"
+	ipvanish_pass = "password"
 
-currency = "ru" # recommended currency for that is Russian Ruble or Argentina Peso, by that you can buy games for lower price
-secondcurrency = 5 # https://stackoverflow.com/a/65653682 
-currency_mark = "pуб."
-pages = 5 # how many pages will parsed
-page_from = 0 # starting from page
-games_limit = 0 # how many games you need to parse | 0 - until pages end.
-sleep_time = 1.3 # time to next card info get
 
-using_proxies = True
 # SCRIPT START #
 
 def ApplyProxy():
-	proxylist = requests.get('https://api.proxyscrape.com?request=getproxies&proxytype=socks5&timeout=1000')
-	proxylist = proxylist.text
-	proxies = str(proxylist).split('\n')
-	try:
-		proxy = str(proxies[random.randint(0, len(proxies))])
-	except:
-		proxy = str(proxies[0])
-	useproxy = str('socks5://'+str(proxy).replace('\r',''))
+	if savedproxies == []:
+		if IpVanish_using is True: 		# ipvanish proxies (private proxies)
+			for host in ipvanish_hosts:
+				savedproxies.append(ipvanish_username+':'+ipvanish_pass+'@'+host+':'+str(ipvanish_port))
+		else: 							# public proxies
+			proxy_service = "https://proxylist.geonode.com/api/proxy-list?limit=200&page=1&sort_by=speed&sort_type=asc&protocols=socks5"
+			proxylist = requests.get(proxy_service)
+			proxies = json.loads(proxylist.text)
+			for proxy in proxies['data']:
+				savedproxies.append(proxy['ip']+':'+proxy['port'])
+	proxy = str(savedproxies[0])
+	savedproxies.pop(0)
+	useproxy = str('socks5://'+str(proxy))
 	proxy = dict(http=useproxy, https=useproxy)
 	return proxy
 
-def ProxyRequest(url, num):
-	pp = 0
-	todisableproxy = 0
-	for g_ in range(999):
-		proxy = ApplyProxy()
-		try:
-			if pp == 0:
-				request = requests.get(url, proxies = proxy)
-			else:
+def LoopRequest(url, num, err_str):
+	if using_proxies is False:
+		for q_ in range(999):
+			try:
 				request = requests.get(url)
-			if request.text == "null":
-				if print_error is True:
-					print(Fore.RED+"STEAM ERROR@"+num+": A lot of requests, retrying with another proxies"+Fore.RESET)
+				if err_str in request.text:
+					if print_error is True:
+						print(Fore.RED+"STEAM ERROR@"+num+": A lot of requests, retrying in 1 minute"+Fore.RESET)
+						time.sleep(60)
+					pass
+				else:
+					return request
+					break
+			except Exception as e:
+				if detailed_errors is True and print_error is True:
+					print(Style.BRIGHT+Fore.BLACK+' ~ '+Style.DIM+Fore.RED+str(e)+Style.RESET_ALL)
+				pass
+	elif using_proxies is True:
+		pp = 0
+		todisableproxy = 0
+		for g_ in range(999):
+			proxy = ApplyProxy()
+			try:
+				if pp == 0:
+					request = requests.get(url, proxies = proxy, timeout = 5)
+				else:
+					request = requests.get(url)
+				if err_str in request.text:
+					if print_error is True:
+						print(Fore.RED+"STEAM ERROR@"+num+": A lot of requests, retrying with another proxies"+Fore.RESET)
+						pp = 0
+					pass
+				else:
+					return request
+					break
+			except Exception as e:
+				if "Max retries exceeded with url" in str(e):
+					if not only_proxies is True:
+						todisableproxy += 1
+						if todisableproxy == 10:
+							pp = 1
+							if print_error is True:
+								print(Style.BRIGHT+Fore.BLACK+' ~ '+Style.DIM+Fore.RED+'Trying request without proxy'+Style.RESET_ALL)
+				elif detailed_errors is True and print_error is True:
+					print(Style.BRIGHT+Fore.BLACK+' ~ '+Style.DIM+Fore.RED+str(e)+Style.RESET_ALL)
 					pp = 0
 				pass
-			else:
-				return request
-				break
-		except Exception as e:
-			if "Max retries exceeded with url" in str(e):
-				todisableproxy += 1
-				if todisableproxy == 10:
-					pp = 1
-					if print_error is True:
-						print(Style.BRIGHT+Fore.BLACK+' ~ '+Style.DIM+Fore.RED+'Trying request without proxy'+Style.RESET_ALL)
-			elif detailed_errors is True and print_error is True:
-				print(Style.BRIGHT+Fore.BLACK+' ~ '+Style.DIM+Fore.RED+str(e)+Style.RESET_ALL)
-				pp = 0
-			pass
-
-def LoopRequest(url, num):
-	for q_ in range(999):
-		try:
-			request = requests.get(url)
-			if request.text == "null":
-				if print_error is True:
-					print(Fore.RED+"STEAM ERROR@"+num+": A lot of requests, retrying with another proxies"+Fore.RESET)
-				pass
-			else:
-				return request
-				break
-		except Exception as e:
-			if detailed_errors is True and print_error is True:
-				print(Style.BRIGHT+Fore.BLACK+' ~ '+Style.DIM+Fore.RED+str(e)+Style.RESET_ALL)
-			pass
+		ProxyRequest(url, num)
 
 games = []
+savedproxies = []
 
 os.system('cls')
 
@@ -100,18 +119,8 @@ for i_ in range(pages):
 	if detalied_actions is True:
 		print(Style.BRIGHT+Fore.BLACK+" ~ === Page "+str(page_from)+" === ~ "+Style.RESET_ALL)
 
-	request = requests.get(url)
+	request = LoopRequest(url, '1', '<meta property="og:title" content="Steam Community :: Error">')
 	
-	if '<meta property="og:title" content="Steam Community :: Error">' in request.text:
-		if print_error is True:
-			print(Fore.RED+"STEAM ERROR@1: A lot of requests, retrying with proxies"+Fore.RESET)
-		if using_proxies is True:
-			ProxyRequest(url, '1')
-		else:
-			time.sleep(65)
-			LoopRequest(url, '1')
-
-
 	splitt = str(request.text).split('data-ds-itemkey="App_')
 	splitt.pop(0)
 
@@ -143,16 +152,7 @@ for gameid in games:
 	if detalied_actions is True:
 		print(Style.BRIGHT+Fore.BLACK+" ~ === Cards from "+gameid['name']+" === ~ "+Style.RESET_ALL)
 
-	request = requests.get(url)
-
-	if '<meta property="og:title" content="Steam Community :: Error">' in request.text:
-		if print_error is True:
-			print(Fore.RED+"STEAM ERROR@2: A lot of requests, retrying with proxies"+Fore.RESET)
-		if using_proxies is True:
-			ProxyRequest(url, '2')
-		else:
-			time.sleep(65)
-			LoopRequest(url, '2')
+	request = LoopRequest(url, '2', '<meta property="og:title" content="Steam Community :: Error">')
 
 	splitt = str(request.text).split('https://steamcommunity.com/market/listings/753/')
 	splitt.pop(0)
@@ -163,16 +163,7 @@ for gameid in games:
 		cardurl = str(re.search('(.*)" id="resultlink_', data).group(1))
 
 		url = 'https://steamcommunity.com/market/priceoverview/?currency='+ str(secondcurrency) +'&appid=753&market_hash_name=' + str(cardurl)
-		request = requests.get(url)
-
-		if request.text == 'null':
-			if print_error is True:
-				print(Fore.RED+"STEAM ERROR@3: A lot of requests"+Fore.RESET)
-			if using_proxies is True:
-				ProxyRequest(url, '3')
-			else:
-				time.sleep(65)
-				LoopRequest(url, '3')
+		request = LoopRequest(url, '3', 'null')
 
 		carddata = json.loads(request.text)
 
@@ -197,7 +188,6 @@ for gameid in games:
 
 print(Fore.BLUE+"FILTERING GAMES."+Fore.RESET)
 # FILTERING GAMES WITH CHEAP CARDS #
-
 text = ''
 for game in games:
 
